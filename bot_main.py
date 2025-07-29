c506245c44a142614397b34f86adb2d1b9446cdb
GerGreta <greta.odessa@gmail.com>
2025-07-29 18:58:19 +0300

    фикс ппп

--- bot_main.py
+++ bot_main.py
@@ -1,11 +1,31 @@
 import asyncio
+import os
+from aiohttp import web
 from handlers import dp, bot
 
-async def main():
-    print("Удаляю webhook (если установлен)...")
-    await bot.delete_webhook()
+async def handle(request):
+    return web.Response(text="OK")
+
+async def start_polling():
     print("Запускаю polling...")
     await dp.start_polling(bot)
 
+async def main():
+    app = web.Application()
+    app.add_routes([web.get('/', handle)])
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.getenv("PORT", 8000))
+    site = web.TCPSite(runner, '0.0.0.0', port)
+    await site.start()
+
+    print(f"HTTP сервер запущен на порту {port}")
+
+    await start_polling()
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот остановлен")
